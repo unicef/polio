39d8b674140590ae47724187fda0b5d89203d4ec
Morlay <morlay@qq.com>
2015-09-09 10:05:26 +0800

    update to jsdom@6.3 and node@4.0

--- webapp/src/helpers/jsdom.js
+++ webapp/src/helpers/jsdom.js
@@ -1,7 +1,5 @@
-// never require this file in frontend code
-
 import { jsdom } from 'jsdom';
 
 global.document = jsdom('<html><body></body></html>');
-global.window = global.document.parentWindow;
-global.navigator = window.navigator || {};
+global.window = global.document.defaultView;
+global.navigator = global.window.navigator || {};
